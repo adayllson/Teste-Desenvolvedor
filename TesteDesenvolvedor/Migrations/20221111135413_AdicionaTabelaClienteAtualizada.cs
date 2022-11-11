c17f2f97896aed730e05a9bbf6b020dfbebc268c
adailson-almeida <adayllson.almeida@gmail.com>
2022-11-11 11:48:23 -0300

    update migrations

---
+++ TesteDesenvolvedor/Migrations/20221111135413_AdicionaTabelaClienteAtualizada.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteDesenvolvedor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaTabelaClienteAtualizada : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Clientes",
+                newName: "CPF");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CPF",
+                table: "Clientes",
+                newName: "Cpf");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Cpf",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
