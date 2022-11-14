5fe835a6d809601fcdf0f4d89a5a2e04c764df88
adailson-almeida <adayllson.almeida@gmail.com>
2022-11-14 10:28:09 -0300

    página criar do imóvel

--- TesteDesenvolvedor/Controllers/ImovelController.cs
+++ TesteDesenvolvedor/Controllers/ImovelController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TesteDesenvolvedor.Context;
 
 namespace TesteDesenvolvedor.Controllers
 {
-    public class ImovelController : Controller
+        public class ImovelController : Controller
     {
-        
+        private readonly ImobiliariaContext _context;
+
+        public ImovelController(ImobiliariaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var imoveis = _context.Imoveis.ToList();
+            return View(imoveis);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
     }
 }
