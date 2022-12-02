3a522e9af8326ed9b87e47ad3beec0cfe3449541
adailson-almeida <adayllson.almeida@gmail.com>
2022-12-02 09:47:36 -0300

    update migrations

---
+++ TesteDesenvolvedor/Migrations/20221202123429_AtualizaTabelaImovel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteDesenvolvedor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizaTabelaImovel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClienteId",
+                table: "Imoveis",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClienteId",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imoveis_ClienteId",
+                table: "Imoveis",
+                column: "ClienteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Imoveis_Clientes_ClienteId",
+                table: "Imoveis",
+                column: "ClienteId",
+                principalTable: "Clientes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Imoveis_Clientes_ClienteId",
+                table: "Imoveis");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Imoveis_ClienteId",
+                table: "Imoveis");
+
+            migrationBuilder.DropColumn(
+                name: "ClienteId",
+                table: "Imoveis");
+
+            migrationBuilder.DropColumn(
+                name: "ClienteId",
+                table: "Clientes");
+        }
+    }
+}
--- TesteDesenvolvedor/Models/Imovel.cs
+++ TesteDesenvolvedor/Models/Imovel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,5 +16,6 @@ namespace TesteDesenvolvedor.Models
         public DateTime DataPublicacao { get; set; }
         public string Descricao { get; set; }
         public string Endereco { get; set; }
+
     }
 }
